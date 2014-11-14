32a407cadb90276ce2361cf89c5e943a9a330e77
Ferhat Ziba <fero.ziba@gmail.com>
2014-11-14 20:16:07 +0100

    remove unused callback

--- app/controllers/fashion_fly_editor/application_controller.rb
+++ app/controllers/fashion_fly_editor/application_controller.rb
@@ -1,14 +1,5 @@
 module FashionFlyEditor
   class ApplicationController < ActionController::Base
-
-    # Nur zum Testen
-    if Rails.env.development?
-      before_filter :call_action
-      def call_action
-        call_hooks("TEST")
-      end
-    end
-
     def call_hooks(collection, options={})
       for hook in FashionFlyEditor::Engine.configuration.callbacks
         ::ApplicationController.method(hook).call(collection, options)
