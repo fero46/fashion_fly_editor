c165436e512522cab71686d162e7ad0d17eb1e73
Bernd Suenkel <bsuenkel@bnerd.de>
2014-12-10 16:07:36 +0100

    adds select correct nested categories

--- app/controllers/fashion_fly_editor/collections_controller.rb
+++ app/controllers/fashion_fly_editor/collections_controller.rb
@@ -17,7 +17,7 @@ module FashionFlyEditor
       end
 
       @collection = Collection.new
-      @categories = Category.all.collect { |c| [c.name, c.id] }
+      @categories = category_options_array([], params["scope"]["id"])
       render layout: false
     end
 
@@ -82,5 +82,19 @@ module FashionFlyEditor
       @redirect_url.gsub(":id", @collection.id.to_s)
     end
 
+    def subcat_prefix(depth)
+      ("&nbsp;" * 2 * depth).html_safe
+    end
+
+    def category_options_array(categories=[], parent_id=nil, depth=0)
+      Category.where(parent_id: parent_id).order(:id).each do |category|
+        categories << [subcat_prefix(depth) + category.name, category.id]
+        category_options_array(categories, category.id, depth+1)
+      end
+
+      categories
+    end
+
   end
+
 end
