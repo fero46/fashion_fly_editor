99d4d9f1f88e4426c85dbe93450f79ea5910128d
Ferhat Ziba <fero.ziba@gmail.com>
2014-12-17 04:42:12 +0100

    hook from the view into mainapp

--- app/controllers/fashion_fly_editor/fashion_fly_editor_controller.rb
+++ app/controllers/fashion_fly_editor/fashion_fly_editor_controller.rb
@@ -2,6 +2,7 @@ module FashionFlyEditor
   class FashionFlyEditorController < ApplicationController #ActionController::Base
 
     helper_method :scoped_current_user
+    helper_method :call_in_mainapp
 
     def call_hooks(collection, options={})
       for hook in FashionFlyEditor::Engine.configuration.callbacks
@@ -11,6 +12,14 @@ module FashionFlyEditor
 
     def scoped_current_user
       call_method(FashionFlyEditor::Engine.configuration.current_user)
+    end
+
+    def call_in_mainapp(method, options={})
+      if options.blank?    
+        ::ApplicationController.method(method).call()
+      else
+        ::ApplicationController.method(method).call(options)
+      end
     end
 
 private
