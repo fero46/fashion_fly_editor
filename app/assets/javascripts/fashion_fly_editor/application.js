ad0a7ada11071124f530a3e7c37520a5e5596531
Bernd Suenkel <bsuenkel@bnerd.de>
2014-12-20 01:29:25 +0100

    adds angular ui-select

--- app/assets/javascripts/fashion_fly_editor/application.js
+++ app/assets/javascripts/fashion_fly_editor/application.js
@@ -4,6 +4,7 @@
 //= require fashion_fly_editor/vendor/angular-resource
 //= require fashion_fly_editor/vendor/angular-sanitize
 //= require fashion_fly_editor/vendor/ngdialog
+//= require fashion_fly_editor/vendor/select
 //= require fashion_fly_editor/tools
 //= require_tree ./angular
 
