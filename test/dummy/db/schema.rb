120cd2656b3a531222c490211f07f2637a7d1595
Ferhat Ziba <fero.ziba@gmail.com>
2015-02-25 22:59:48 +0100

    fix type

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141218155820) do
+ActiveRecord::Schema.define(version: 20150225215930) do
 
-  create_table "fashion_fly_editor_categories", force: true do |t|
-    t.string   "name"
-    t.string   "slug"
+  create_table "fashion_fly_editor_categories", force: :cascade do |t|
+    t.string   "name",        limit: 255
+    t.string   "slug",        limit: 255
     t.integer  "parent_id"
-    t.string   "parent_type"
+    t.string   "parent_type", limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 20141218155820) do
   add_index "fashion_fly_editor_categories", ["parent_id", "parent_type"], name: "category_parent"
   add_index "fashion_fly_editor_categories", ["slug"], name: "index_fashion_fly_editor_categories_on_slug", unique: true
 
-  create_table "fashion_fly_editor_collection_items", force: true do |t|
+  create_table "fashion_fly_editor_collection_items", force: :cascade do |t|
     t.integer  "collection_id"
     t.integer  "item_id"
     t.integer  "position_x"
@@ -35,16 +35,16 @@ ActiveRecord::Schema.define(version: 20141218155820) do
     t.float    "rotation"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "image"
+    t.string   "image",         limit: 255
     t.integer  "scale_x"
     t.integer  "scale_y"
-    t.integer  "order",         default: 0
+    t.integer  "order",                     default: 0
   end
 
-  create_table "fashion_fly_editor_collections", force: true do |t|
-    t.string   "title"
-    t.string   "description"
-    t.string   "image"
+  create_table "fashion_fly_editor_collections", force: :cascade do |t|
+    t.string   "title",       limit: 255
+    t.text     "description", limit: 255
+    t.string   "image",       limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "category_id"
@@ -56,10 +56,10 @@ ActiveRecord::Schema.define(version: 20141218155820) do
   add_index "fashion_fly_editor_collections", ["category_id"], name: "index_fashion_fly_editor_collections_on_category_id"
   add_index "fashion_fly_editor_collections", ["user_id"], name: "index_fashion_fly_editor_collections_on_user_id"
 
-  create_table "fashion_fly_editor_subscriptions", force: true do |t|
+  create_table "fashion_fly_editor_subscriptions", force: :cascade do |t|
     t.integer  "collection_id"
     t.integer  "subscriber_id"
-    t.string   "subscriber_type"
+    t.string   "subscriber_type", limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -67,7 +67,7 @@ ActiveRecord::Schema.define(version: 20141218155820) do
   add_index "fashion_fly_editor_subscriptions", ["collection_id"], name: "index_fashion_fly_editor_subscriptions_on_collection_id"
   add_index "fashion_fly_editor_subscriptions", ["subscriber_id", "subscriber_type"], name: "subscriber"
 
-  create_table "scopes", force: true do |t|
+  create_table "scopes", force: :cascade do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
